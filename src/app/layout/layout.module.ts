c58cf46073cde7bd4565c0eee0c240b632b39bd0
GS703 <gs703@live.mdx.ac.uk>
2017-01-27 20:22:28 +0100

    Add layout home component

---
+++ src/app/layout/layout.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './layout-home/footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [FooterComponent]
+})
+export class LayoutModule { }
