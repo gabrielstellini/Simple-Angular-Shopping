873dfcdb320c79d0bbbe0ad025e7bd433c48918d
GS703 <gs703@live.mdx.ac.uk>
2017-02-02 21:58:20 +0100

    Update login auth

--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -16,20 +16,25 @@ export class AuthService {
 
   }
 
-  login(username:string, password:string){
+  login(email:string, password:string):number{
     //TODO: AttemFor debugging purposes -pt database login here
     //Assume username 1 is normal, username 2 is admin
 
-    if(username === "1"){
+    if(email === "demo@123.123"){
       this.currUser.username = "1";
       this.currUser.isAdmin = false;
       this.currUser.authGuid = "e58607e4-af33-46ed-8fee-42225c34c2c5";
+      return 202;
+
     }
-    else if(username === "2"){
+    else if(email === "demo@321.321"){
       this.currUser.username = "2";
       this.currUser.isAdmin = true;
       this.currUser.authGuid = "5e5325df-6b26-48fc-b44a-0497bb09209f";
+      return 202;
     }
+
+    return 404;
   }
 
   signout(){
