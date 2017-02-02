85eb679f2d3a4c4efb1c151bba032baab6c3312d
GS703 <gs703@live.mdx.ac.uk>
2017-02-02 21:58:10 +0100

    Add login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../shared/services/auth.service";
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  loginData = {username: "", password: ""}
+
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {
+  }
+
+  attemptLogin(){
+    let username = this.loginData.username;
+    let password = this.loginData.password;
+
+    let result = this.authService.login(username,password);
+    if(result === 202){
+      console.log("success!");
+    }
+    else if(result === 404){
+      console.log("Fail!");
+    }
   }
 
 }
