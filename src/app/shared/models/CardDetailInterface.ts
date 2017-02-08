c688d709a5eac972faaf45b71db7efa59df6a492
Gabriel <GS703@live.mdx.ac.uk>
2017-02-08 01:57:10 +0100

    Add credit card detail interface

---
+++ src/app/shared/models/CardDetailInterface.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by gabriel on 08/02/2017.
+ */
+
+export interface CardDetailInterface {
+  gender: "Mr" | "Ms",
+  cardName: string,
+  cardNumber: number,
+  date:any,
+  cvv:number,
+  country:string
+}
+
