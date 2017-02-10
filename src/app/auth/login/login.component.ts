ad5f45cba59a30794a0b5fc199ba2c6fe5f0571f
Gabriel <GS703@live.mdx.ac.uk>
2017-02-10 21:41:31 +0100

    Add login rerouting

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {AuthService} from "../../shared/services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login',
@@ -8,10 +9,10 @@ import {AuthService} from "../../shared/services/auth.service";
 })
 export class LoginComponent implements OnInit {
 
-
   loginData = {username: "", password: ""}
 
-  constructor(private authService:AuthService) { }
+  constructor(private authService:AuthService,
+  private router:Router) { }
 
   ngOnInit() {
   }
@@ -23,10 +24,10 @@ export class LoginComponent implements OnInit {
     let result = this.authService.login(username,password);
     if(result === 202){
       console.log("success!");
+      this.router.navigate(['/items']);
     }
     else if(result === 404){
       console.log("Fail!");
     }
   }
-
 }
